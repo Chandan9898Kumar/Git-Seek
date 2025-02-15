706673a0750159d841af04cd0868f90babd5ad41
Chandan <Chandan17ee@gmail.com>
2025-02-15 16:27:30 +0530

    chore: added seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
+});
